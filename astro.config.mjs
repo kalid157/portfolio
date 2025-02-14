15e9253d51c18c0e49c480613b8863c59763d311
kalid Djele <75723653+kalid157@users.noreply.github.com>
2025-02-14 21:45:06 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 // @ts-check
-//import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-//export default defineConfig({
-
-  
- // base: '/portfolio/',
-  
-
-//});
 import { defineConfig } from 'astro/config';
 
+// https://astro.build/config
 export default defineConfig({
-  outDir: './dist',
+  
+  base: '/portfolio/',  
+
 });
+
