030dce08f59e5b891c94294a8172d2189f82d6bc
djelekalid <lid.kalidjele3@gmail.com>
2025-02-14 16:17:30 +0000

    modification du lien chemin css ./_astro/about.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 
-  site: 'https://github.com/kalid157/portfolio',
   
-  trailingSlash: 'always',
-
-
+  base: 'portfolio',
+  
 
 });
