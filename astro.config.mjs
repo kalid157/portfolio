c5c5bca83c5a33a196d29b85453b3307e1d5926c
djelekalid <lid.kalidjele3@gmail.com>
2025-02-15 05:39:11 +0000

    Outdir

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-<<<<<<< HEAD
 
-   base: '',
-  
-=======
-  
-  base: '/portfolio/',  
-
->>>>>>> 3c9efcb223b33cbef204b58db0a135adbd1fa966
+   base: '/portfolio/',
+   outDir: './docs',
 });
-
