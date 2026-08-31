f1f56cf90686ca39562a6b56b9786e3429da7a35
kalid Djele <75723653+kalid157@users.noreply.github.com>
2026-08-31 09:18:09 -0400

    Update base path conffix base path pour netlifyiguration for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 
-   base: '/portfolio/',
+  // base: '/portfolio/',
+   base: process.env.NETLIFY ? '/' : '/portfolio/',
    outDir: './docs',
 });
